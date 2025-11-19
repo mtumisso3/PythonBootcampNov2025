82ebac68c23f985ecfc46f17da72a69bab3e26c8
mtumisso3 <96927170+mtumisso3@users.noreply.github.com>
2025-11-19 17:49:22 +0000

    Add PythonBootcampSetup and remove main.py

---
+++ PythonBootcampSetup.py
@@ -0,0 +1,5 @@
+# Write "Hello World!" -- day pne Pythond Programming
+print("Hello World!")
+print("The journy has begun.")
+print("Feels so wonderful to start learning Python and utilizing AI models to debugg errors when araise that is enhancing productivity.")
+print("This is really fantastic!")
